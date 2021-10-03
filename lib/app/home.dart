9ad9e584adb85657e0f3d4493194db77ff030d43
GabrielPauloT <gabrielpt201111@hotmail.com>
2021-10-03 15:17:08 -0300

    Barra lateral começo

--- lib/app/home.dart
+++ lib/app/home.dart
@@ -1,15 +1,62 @@
+// ignore_for_file: prefer_const_constructors, unnecessary_new
+
 import 'package:flutter/material.dart';
+import 'package:fluttericon/font_awesome_icons.dart';
 
 class HomeI extends StatelessWidget {
   const HomeI({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView(
-        children: [
-          Text("teste"),
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+            );
+          },
+        ),
+        title:
+            Text("Tech Motors", style: TextStyle(fontWeight: FontWeight.bold)),
+        backgroundColor: Color(0xF513B5EB),
+      ),
+      drawer: new Drawer(
+        child: ListView(children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountName: new Text(
+              "Financeiro",
+              style: new TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 17.0,
+              ),
+            ),
+            /* accountEmail: new Text("TechMotrs@tech.com.br"), */
+            currentAccountPicture: Image.asset('assets/imagens/download.jpeg'),
+            decoration: new BoxDecoration(color: Color(0xF513B5EB)),
+            accountEmail: null,
+          ),
+          ListTile(
+            leading: Icon(FontAwesome.asl_interpreting),
+            title: Text("Contas a receber"),
+            /* onTap: () async {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Profile()));
+              }, */
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text("Contas a pagar"),
+            /* onTap: () async {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Profile()));
+              }, */
+          )
+        ]),
       ),
     );
   }
